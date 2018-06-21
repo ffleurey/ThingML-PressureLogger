80b04032519cac430369ff177a324fdbb1025384
Franck Fleurey <franck@fleurey.com>
2018-06-21 14:31:33 +0200

    cleanup

--- thingml-gen/java/JavaClientGateway/src/main/java/org/thingml/generated/api/IJavaClient_sensor.java
+++
@@ -1,7 +0,0 @@
-package org.thingml.generated.api;
-
-import org.thingml.generated.api.*;
-
-public interface IJavaClient_sensor{
-void pressure_via_sensor(int LoggerMsgs_pressure_a_var, int LoggerMsgs_pressure_b_var);
-}
